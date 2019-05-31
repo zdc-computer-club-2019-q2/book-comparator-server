07b43a76c9704aac58f08326a1ff5e2d655cf755
Timothee Groleau <tgroleau@zendesk.com>
2019-05-31 18:08:20 +0800

    handle /search through kinokuniya

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var request = require('request');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+router.get('/search', function(req, res, next) {
+	request(`https://singapore.kinokuniya.com/bw/${req.query.isbn}`, (err, kino) => {
+		var m1 = kino.body.match(/<meta property="og:title" content="([^"]+)"/);
+		var m2 = kino.body.match(/<meta property="og:description" content="([^"]+)"/);
+
+		res.json({
+			isbn: req.query.isbn,
+			title: m1[1],
+			description: m2[1],
+		});
+	});
+});
+
+module.exports = router;	
