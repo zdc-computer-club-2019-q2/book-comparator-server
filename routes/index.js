858b2fdb38434df3e9ef6cbd782931009d33e157
Glitch (book-comparator) <none>
2019-07-26 09:11:42 +0000

    💰🎏 Checkpoint ./routes/index.js:497590/840:4118873/184:4118884/4647:4118885/204:4120702/435:5737403/620:5737404/5 ./chat:497590/89

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,10 @@ router.get('/search', function(req, res, next) {
       { "site": "", "price": 105, "url": "" }
     ],
     "description": "A dystop",
-    "author": "George Orwell",
+    "authors": [
+      "George Orwell",
+      "Another author"
+    ],
     "recommended": [
       { "isbn": "29384792837", "title": "Another book", "description" },
     ],
@@ -50,15 +53,23 @@ router.get('/search', function(req, res, next) {
   */
   function done() {
     if ('bookdepository' in offers && 'kinokuniya' in offers) {
+      response.offers = Object.keys(offers).map(key => {
+        const value = offers[key];
+        return {
+          ...value,
+          site: key
+        };
+      });
       res.json(response)
     }
   }
+  
 	request(`https://singapore.kinokuniya.com/bw/${req.query.isbn}`, (err, kino) => {
 		//var m1 = kino.body.match(/<meta property="og:title" content="([^"]+)"/);
 		//var m2 = kino.body.match(/<meta property="og:description" content="([^"]+)"/);
 		const doc = (new JSDOM(kino.body)).window;
 		
-		var kino_title = (doc.document.getElementsByClassName('dContent')[0].querySelector('h1').textContent);
+		var kino_title = (doc.document.querySelector('.dContent h1').textContent);
 		var kino_desc = (doc.document.getElementsByClassName('dInfoTxt product-descrip readmore-js-section readmore-js-collapsed')[0].textContent);
 		var kino_authors = (doc.document.getElementsByClassName('author')[0].querySelectorAll('a'));
 		var kino_category = (doc.document.getElementsByClassName('clearfix')[0].querySelector('li:nth-child(2)').textContent);
@@ -66,26 +77,12 @@ router.get('/search', function(req, res, next) {
 		//console.log(price);
 		var img = (doc.document.getElementsByClassName('slider3')[0].querySelector('img'));
 		
-		var kino_authors_array = [];
-		Array.from(kino_authors).forEach(function(m){
-			kino_authors_array.push(m.textContent);
-		})
-    
     response.isbn = req.query.isbn;
     response.title = kino_title;
     response.cover = img.src;
     response.category = kino_category;
     response.description = kino_desc.trim();
-    response.author = kino_authors_array;
-    
-    // res.json({
-    //   isbn: req.query.isbn,
-    //   title: kino_title,
-    //   cover: img.src,
-    //   category: kino_category,
-    //   description: kino_desc.trim(),
-    //   author: kino_authors_array,
-    // });
+    response.author = Array.from(kino_authors).map(author => author.text);
     
     offers.kinokuniya = {
       price: price,
@@ -96,15 +93,23 @@ router.get('/search', function(req, res, next) {
 	});
   
   request(`https://www.bookdepository.com/search?searchTerm=${req.query.isbn}`, (err, depo) => {
-		const depo_dom = new JSDOM(depo.body)
-		// ser = depo_dom.serialize()
-		// console.log(ser)
+		const depo_dom = new JSDOM(depo.body);
 		const book_price = depo_dom.window.document.querySelector('.sale-price').textContent;
     
 		offers.bookdepository = {
       price: book_price,
-      url: depo_dom.window.document.querySelector('link[rel="canonical"]').href
+      url: 'https://www.bookdepository.com' + depo_dom.window.document.querySelector('link[rel="canonical"]').href
     };
+    
+    done();
+  });
+});
+
+router.get('/amazon', function(req, res, next) {
+	request(`https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Dstripbooks-intl-ship&field-keywords=${req.query.isbn}`, (err, emma) => {
+		let response = { 'body': emma.body}
+    
+		res.json(response)
     
   });
 });
