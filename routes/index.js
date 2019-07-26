e7aff6e470a4d112555fb5732d31e034c3e59420
Glitch (book-comparator) <none>
2019-07-26 08:27:43 +0000

    🍣🌖 Checkpoint ./routes/index.js:497590/32:4118873/492:4120702/2160 ./package.json:497590/24:4120702/775 ./chat:497590/59

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 var request = require('request');
 
 /* GET home page. */
@@ -10,15 +11,41 @@ router.get('/', function(req, res, next) {
 
 router.get('/search', function(req, res, next) {
 	request(`https://singapore.kinokuniya.com/bw/${req.query.isbn}`, (err, kino) => {
-		var m1 = kino.body.match(/<meta property="og:title" content="([^"]+)"/);
-		var m2 = kino.body.match(/<meta property="og:description" content="([^"]+)"/);
-
+		//var m1 = kino.body.match(/<meta property="og:title" content="([^"]+)"/);
+		//var m2 = kino.body.match(/<meta property="og:description" content="([^"]+)"/);
+		const doc = (new JSDOM(kino.body)).window;
+		
+		var m1 = (doc.document.getElementsByClassName('dContent')[0].querySelector('h1').textContent);
+		var m2 = (doc.document.getElementsByClassName('dInfoTxt product-descrip readmore-js-section readmore-js-collapsed')[0].textContent);
+		var m3 = (doc.document.getElementsByClassName('author')[0].querySelectorAll('a'));
+		var m4 = (doc.document.getElementsByClassName('clearfix')[0].querySelector('li:nth-child(2)').textContent);
+		//var price = (doc.document.getElementsByClassName('price')[0].querySelector('span').textContent);
+		//console.log(price);
+		var img = (doc.document.getElementsByClassName('slider3')[0].querySelector('img'));
+		
+		var authors_array = [];
+		Array.from(m3).forEach(function(m){
+			authors_array.push(m.textContent);
+		})
 		res.json({
 			isbn: req.query.isbn,
-			title: m1[1],
-			description: m2[1],
+			title: m1,
+			cover: img.src,
+			category: m4,
+			description: m2.trim(),
+			author: authors_array,
 		});
 	});
+  
+  request(`https://www.bookdepository.com/search?searchTerm=${req.query.isbn}`, (err, depo) => {
+		const depo_dom = new JSDOM(depo.body)
+		// ser = depo_dom.serialize()
+		// console.log(ser)
+		book_title = depo_dom.window.document.querySelector('.item-info').querySelector('h1[itemprop="name"').textContent;
+		console.log(book_title)
+		book_price = depo_dom.window.document.querySelector('.sale-price').textContent
+		console.log(book_price)
+		price_list["bookdepository"] = book_price
 });
 
 module.exports = router;	
